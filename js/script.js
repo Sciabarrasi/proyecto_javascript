92a81c5592af0f5a0c59076294eecacfac69f9a4
Sciabarrasi <gianluca_sciabarrasi@hotmail.com>
2023-04-13 03:54:59 -0300

    Hago un primer push de mi proyecto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let nombreUsuario = prompt("Por favor, ingrese su nombre");
+finCompra(nombreUsuario);
+
+
+function finCompra(nombreUsuario){
+    alert('Gracias por tu compra', + nombreUsuario,' !');
+}
+
